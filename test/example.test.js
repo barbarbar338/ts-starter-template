2674aaf3018283c94981166397dc3aa7400c3df4
barbarbar338 <demirci.baris38@gmail.com>
2020-08-15 22:10:17 +0300

    🐛 Better test environment

--- test/example.test.js
+++
@@ -1,5 +0,0 @@
-describe("Example Test", () => {
-    it("Should pass", (done) => {
-        done();
-    });
-});
---
+++ tests/example.test.ts
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+describe("Example Test", () => {
+    it("Should pass", () => {
+        expect(true);
+    });
+});
