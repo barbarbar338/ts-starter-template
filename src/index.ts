414c75b49777e9cc3c58db8edb72224d5cb35cde
barbarbar338 <demirci.baris38@gmail.com>
2020-08-15 21:48:41 +0300

    ✨ First Commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function example(str: string): void {
+    console.log(str);
+}
+const str = "This is a test string";
+example(str);
---
+++ test/example.test.js
@@ -0,0 +1,5 @@
+describe("Example Test", () => {
+    it("Should pass", (done) => {
+        done();
+    });
+});
